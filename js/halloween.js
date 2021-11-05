1341f27e94f907a17712a9411947343e26a79930
Muro Bakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-11-05 14:11:56 +0400

    swiper save 1

--- js/halloween.js
+++ js/halloween.js
@@ -1,20 +1,5 @@
-// const navMenu = document.querySelector('.nav_container'),
-//       navToggle = document.querySelector('.nav_toggle'),
-//       navClose = document.querySelector('.nav_close')
 
-// // SHOW MENU
-// if(navToggle){
-//   navToggle.addEventListener('click', () => {
-//     navMenu.classList.add('show_menu')
-//   })
-// }
-
-// // HIDE MENU
-// if(navClose){
-//   navClose.addEventListener('click', () => {
-//     navMenu.classList.remove('show_menu')
-//   })
-// }
+/* ==========         TOGGLE MENU         ===========*/
 
 const   navMenu = document.getElementById("nav-container"),
         navToggle = document.getElementById("nav-toggle"),
@@ -36,4 +21,16 @@ if (xClose) {
     })
 }
 
+/* ==========         HOME SWIPER         ===========*/
+
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: "true",
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
 
